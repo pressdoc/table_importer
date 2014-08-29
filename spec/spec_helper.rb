3e0f74da74fd906c0088163f3d37a7888ef5bed1
Nick Dowse <nm.dowse@gmail.com>
2014-08-29 17:07:55 +0200

    Moved to CodeClimate test coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,17 @@
-require 'coveralls'
 require 'table_importer'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require 'rubygems'
 require 'active_support/all'
 require 'table_importer/exceptions'
-Coveralls.wear!
 
-  # From rspec generator
+
+# From rspec generator
 ENV["RAILS_ENV"] ||= 'test'
 require 'rspec/autorun'
-
+if ENV["RAILS_ENV"] == "test"
+  ENV["CODECLIMATE_REPO_TOKEN"] = a64b61ae04b5803d17610e71b1c55ff99afa1e540fa05cde9077ad663b8eb242
+end
 RSpec.configure do |config|
   config.order = "random"
 end
