0fb0158ad5cfb5823746ef464cc7a4b8084334bd
Nick Dowse <nm.dowse@gmail.com>
2014-08-29 17:52:06 +0200

    Moved to CodeClimate test coverage. Again.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ require 'table_importer/exceptions'
 ENV["RAILS_ENV"] ||= 'test'
 require 'rspec/autorun'
 if ENV["RAILS_ENV"] == "test"
-  ENV["CODECLIMATE_REPO_TOKEN"] = a64b61ae04b5803d17610e71b1c55ff99afa1e540fa05cde9077ad663b8eb242
+  ENV["CODECLIMATE_REPO_TOKEN"] = 'a64b61ae04b5803d17610e71b1c55ff99afa1e540fa05cde9077ad663b8eb242'
 end
 RSpec.configure do |config|
   config.order = "random"
