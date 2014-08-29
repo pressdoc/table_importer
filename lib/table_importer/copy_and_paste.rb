cca54858ab8bbda7e8a9c77379c5627eb5ec8a84
Nick Dowse <nm.dowse@gmail.com>
2014-08-29 10:22:54 +0200

    Refactoring copy and paste.

--- lib/table_importer/copy_and_paste.rb
+++ lib/table_importer/copy_and_paste.rb
@@ -3,17 +3,40 @@ module TableImporter
   class CopyAndPaste < Source
 
     def initialize(data)
-      @data = data[:content]
-      raise Exceptions::EmptyFileImportError.new if @data.blank? || @data[0..100].gsub(/[^A-Za-z0-9]/, '').blank?
-      @data.gsub!(/\r\n|\r/, "\n")
-      @column_separator = SEPARATORS[data[:column_separator].to_sym] if !data[:column_separator].nil?
-      @record_separator = SEPARATORS[data[:record_separator].to_sym] if !data[:record_separator].nil?
+      @data = assign_data(data[:content])
+      @column_separator, @record_separator = assign_separators(data[:col_sep], data[:rec_sep])
+      @headers, @headers_present = assign_headers(data[:headers], data[:headers_present])
       @compulsory_headers = data[:compulsory_headers]
-      @headers = data[:headers]
-      @headers_present = data[:headers_present]
-      data_conforms_pattern
       @delete_empty_columns = @data.length < 50000
-      @headers = @headers_present ? get_first_line : get_headers if data[:headers].blank?
+    end
+
+    def assign_data(content)
+      raise Exceptions::EmptyFileImportError.new if content.blank? || content[0..100].gsub(/[^A-Za-z0-9]/, '').blank?
+      content.gsub!(/\r\n|\r/, "\n")
+      return content
+    end
+
+    def assign_separators(col_sep, rec_sep)
+      col_sep = SEPARATORS[col_sep.to_sym] if !col_sep.nil?
+      rec_sep = SEPARATORS[rec_sep.to_sym] if !rec_sep.nil?
+      col_sep, rec_sep = data_conforms_pattern(col_sep, rec_sep)
+    end
+
+    def data_conforms_pattern(col_sep, rec_sep)
+      # Check to see if data is of bcc style
+      first_item = @data.split(",").first
+      if first_item.present? && first_item.match(/\S@\S/)
+        if first_item.match(/<(\S+@\S+)/)
+          rec_sep ||= ">, "
+          col_sep ||= " <"
+        end
+      end
+      return col_sep, rec_sep
+    end
+
+    def assign_headers(headers, headers_present)
+      headers = headers_present ? get_first_line : get_headers if headers.blank?
+      return headers, headers_present
     end
 
     def get_first_line
@@ -54,17 +77,6 @@ module TableImporter
       mapped_lines[start_point..(start_point+number_of_lines)]
     end
 
-    def convert_headers(provided_headers, mapped_headers, headers_present)
-      new_headers = headers_present ? provided_headers : default_headers
-      new_headers = default_headers(new_headers.count)
-      mapped_headers.each do |key, value|
-        if value.to_i.to_s == value
-          new_headers[value.to_i] = key.to_sym
-        end
-      end
-      new_headers
-    end
-
     def remove_whitespace(column)
       column.each do |column_item|
         column_item.strip!
@@ -76,6 +88,17 @@ module TableImporter
       @headers = convert_headers(get_first_line, @headers, @headers_present)
       lines = get_lines(0, -1).in_groups_of(chunk_size, false)
       clean_chunks(lines, @compulsory_headers)
+    end
+
+    def convert_headers(provided_headers, mapped_headers, headers_present)
+      new_headers = headers_present ? provided_headers : default_headers
+      new_headers = default_headers(new_headers.count)
+      mapped_headers.each do |key, value|
+        if value.to_i.to_s == value
+          new_headers[value.to_i] = key.to_sym
+        end
+      end
+      new_headers
     end
 
     def get_column_separator(first_line = @data)
@@ -90,17 +113,6 @@ module TableImporter
       separators = get_sep_count(first_line)
       separators.reject!{ |sep| sep.keys[0] == get_column_separator}
       @record_separator = sort_separators(separators)
-    end
-
-    def data_conforms_pattern
-      # data is probably a list of emails
-      first_item = @data.split(",").first
-      if first_item.present? && first_item.match(/\S@\S/)
-        if first_item.match(/<(\S+@\S+)/)
-          @record_separator ||= ">, "
-          @column_separator ||= " <"
-        end
-      end
     end
   end
 end
--- spec/copy_and_paste_spec.rb
+++ spec/copy_and_paste_spec.rb
@@ -8,30 +8,30 @@ describe TableImporter::Source do
   context 'when source is a string it' do
 
     before(:each) do
-      @source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :column_separator => "", :record_separator => "", :compulsory_headers => {:email => true}})
+      @source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :col_sep => "", :rec_sep => "", :compulsory_headers => {:email => true}})
     end
 
     it "creates a source object" do
-      TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :column_separator => "", :record_separator => "", :compulsory_headers => {:email => true}})
+      TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :col_sep => "", :rec_sep => "", :compulsory_headers => {:email => true}})
     end
 
     it "gets the correct copy and paste chunks" do
-      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :column_separator => :space, :record_separator => :comma, :compulsory_headers => {:email => true}})
+      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :col_sep => :space, :rec_sep => :comma, :compulsory_headers => {:email => true}})
       source.get_chunks.first[:lines].first[:email].should eql("nick@pr.co")
     end
 
     it "has the correct number of lines" do
-      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :column_separator => :space, :record_separator => :comma, :compulsory_headers => {:email => true}})
+      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :col_sep => :space, :rec_sep => :comma, :compulsory_headers => {:email => true}})
       source.get_chunks(1).count.should eql(3)
     end
 
     it "has the correct number of chunks" do
-      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :column_separator => :space, :record_separator => :comma, :compulsory_headers => {:email => true}})
+      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :col_sep => :space, :rec_sep => :comma, :compulsory_headers => {:email => true}})
       source.get_chunks(2).count.should eql(2)
     end
 
     it "does not have extra spaces in the final chunk" do
-      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :column_separator => :space, :record_separator => :comma, :compulsory_headers => {:email => true}})
+      source = TableImporter::Source.new({:content => "nick@pr.co, dennis@pr.co, lorenzo@pr.co", :headers_present => false, :headers => {"first_name"=>"", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"0", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :col_sep => :space, :rec_sep => :comma, :compulsory_headers => {:email => true}})
       last_chunk = source.get_chunks(2).last
       (last_chunk[:lines].count + last_chunk[:errors].count).should eql(1)
     end
@@ -60,7 +60,7 @@ describe TableImporter::Source do
   context 'when source is a different string' do
 
     before(:each) do
-      @source = TableImporter::Source.new({:content => "Nick Dowse <nick@pr.co>, Dennis van der Vliet <dennis@pr.co>, Jeroen Bos <jeroen@pr.co>", :headers_present => false, :headers => {"first_name"=>"0", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"1", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :column_separator => "", :record_separator => "", :compulsory_headers => {:email => true}})
+      @source = TableImporter::Source.new({:content => "Nick Dowse <nick@pr.co>, Dennis van der Vliet <dennis@pr.co>, Jeroen Bos <jeroen@pr.co>", :headers_present => false, :headers => {"first_name"=>"0", "last_name"=>"", "salutation"=>"", "tag_list"=>"", "email"=>"1", "organization"=>"", "url"=>"", "phone"=>"", "job_title"=>"", "second_url"=>"", "notes"=>"", "twitter_username"=>"", "skype_username"=>"", "pinterest_username"=>"", "instagram_username"=>"", "facebook_username"=>"", "last_name_prefix"=>"", "second_email"=>"", "phone_mobile"=>"", "street"=>"", "street_number"=>"", "zipcode"=>"", "city"=>"", "country"=>""}, :user_headers => nil, :type => "copy_and_paste", :col_sep => "", :rec_sep => "", :compulsory_headers => {:email => true}})
     end
 
     it "gets the correct chunks" do
@@ -105,7 +105,7 @@ describe TableImporter::Source do
 
           lorenzo,\"lorenzo@pr.co\"
           HÐ, “nick¯â@test”, ¾,€",
-        :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :column_separator => :comma, :record_separator => :newline_mac, :compulsory_headers => {:email => true}})
+        :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :col_sep => :comma, :rec_sep => :newline_mac, :compulsory_headers => {:email => true}})
     end
 
     it "has the correct number of lines" do
@@ -129,7 +129,7 @@ describe TableImporter::Source do
   context 'when string is empty' do
     it 'raises an error when creating a source object' do
       expect{
-        TableImporter::Source.new({:content => "", :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :column_separator => :comma, :record_separator => :newline_mac, :compulsory_headers => {:email => true}})
+        TableImporter::Source.new({:content => "", :headers_present => false, :headers => nil, :user_headers => nil, :type => "copy_and_paste", :col_sep => :comma, :rec_sep => :newline_mac, :compulsory_headers => {:email => true}})
       }.to raise_error(Exceptions::EmptyFileImportError)
     end
   end
